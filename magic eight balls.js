3c0311e989efb02f828862472bb2426bb58318fe
Bibek Hanal <bbekhamal26@gmail.com>
2025-02-27 22:18:27 +1100

    first commit

---
+++ magic eight balls.js
@@ -0,0 +1,7 @@
+let userName = 'bibek';
+let userQuestion = 'is that';
+userName = true ? console.log('Hello'+ ' ' + userName) : console.log('Hello');
+// console.log('What' + ' ' + userQuestion +' ' + userName);
+console.log(`${userName} asked: ${userQuestion}`);
+ 
+
