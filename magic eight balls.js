ce7725f6fc9ff943daf88c2ae35c18d5c55ce1c2
Bibek Hanal <bbekhamal26@gmail.com>
2025-02-28 08:13:47 +1100

    update switch statement in magic eight balls

--- magic eight balls.js
+++ magic eight balls.js
@@ -2,44 +2,40 @@ let userName = 'Bibek';
 // userName = true ? console.log('Hello'+ ' ' + userName) : console.log('Hello');
 userName ? console.log(`Hello, ${userName}`) : console.log('Hello');
 
-let userQuestion = ' what number?';
+let userQuestion = ' will I get married?';
 console.log(`${userName} asked = ${userQuestion}`);
 
-let randomNumber = Math.floor(Math.random() * 7);
+let randomNumber = Math.floor(Math.random() * 8);
+console.log(randomNumber);
 
-let eightBall = 6;
-switch (eightBall) {
-  case (0):
-    console.log('It is certain');
+let eightBall = '';
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
     break;
-  case (1):
-    console.log('It is decidedly so');
+  case 1:
+    eightBall = 'It is decidedly so';
     break;
-  case (2):
-    console.log('Reply hazy try again');
+  case 2:
+    eightBall = 'Reply hazy try again';
     break;
-  case (3):
-    console.log('Cannot predict now');
+  case 3:
+    eightBall = 'Cannot predict now';
     break;
-  case (4):
-    console.log('Do not count on it');
+  case 4:
+    eightBall = 'Do not count on it';
     break;
-  case (5):
-    console.log('My sources say no');
+  case 5:
+    eightBall = 'My sources say no';
     break;
-  case (6):
-    console.log('Outlook not so good');
+  case 6:
+    eightBall = 'Outlook not so good';
     break;
-  case (7):
-    console.log('Signs point to yes');
-    break;
-  default:
-    console.log('It is not certain');
+  case 7:
+    eightBall = 'Signs point to yes';
     break;
 
 }
 
-
-
-
-
+console.log(`The magic eight ball says, ${eightBall}.`) 
